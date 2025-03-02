1425b8ca04f0237530a09525563dc85516093dde
yyR <3359754420@qq.com>
2025-03-02 16:07:35 +0800

    home-懒加载

--- src/main.js
+++ src/main.js
@@ -6,11 +6,8 @@ import router from './router'
 
 // 引入初始化样式文件
 import '@/styles/common.scss'
-// 测试接口函数
-// import { getCategoryAPI } from "@/apis/testAPI"
-// getCategoryAPI().then(res => {
-//   console.log(res)
-// })
+import { useIntersectionObserver } from '@vueuse/core'
+
 
 const app = createApp(App)
 
@@ -18,3 +15,21 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
+//定义全局指令
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    // el: 指令绑定的那个元素 img
+    // binding: binding.value 指令等于号后面绑定的值 图片url
+    useIntersectionObserver(
+      el,
+      ([{ isIntersecting }]) => {
+        console.log(isIntersecting)
+        if (isIntersecting) {
+          el.src = binding.value
+        }
+      }
+    )
+  }
+})

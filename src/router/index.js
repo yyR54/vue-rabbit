6c635aa5672226086f21a7981e3ebf7a15364462
yyR <3359754420@qq.com>
2025-03-03 10:58:01 +0800

    二级分类-整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 
 const router = createRouter({
@@ -25,6 +26,10 @@ const router = createRouter({
           path: 'category/:id',
           name: 'category',
           component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
         }
       ]
     },

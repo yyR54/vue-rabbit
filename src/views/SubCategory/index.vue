f07ba1a2d315ab35c0dc2e05f53240c71348386a
yyR <3359754420@qq.com>
2025-03-03 11:26:29 +0800

    二级分类-面包屑导航渲染

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,5 +1,17 @@
 <script setup>
+import { getCategoryFilterAPI } from '@/apis/category'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 
+// 获取面包屑导航数据
+const filterData = ref({})
+const route = useRoute()
+const getFilterData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id)
+  // console.log(res)
+  filterData.value = res.result
+}
+onMounted(() => getFilterData()) 
 
 </script>
 
@@ -9,11 +21,12 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家
-        </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+         <el-breadcrumb-item :to="{ path: `/category/${filterData.parentId}` }">{{ filterData.parentName }}
+      </el-breadcrumb-item>
+         <el-breadcrumb-item>{{ filterData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
+
     <div class="sub-container">
       <el-tabs>
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>

8e7fe913eb0e7458ed18766e283d43d8da6d43a0
yyR <3359754420@qq.com>
2025-03-04 18:37:19 +0800

    登录-表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,28 @@
 <script setup>
+import { ref } from 'vue'
+// 表单数据对象
+const form = ref({
+  account: '',
+  password: ''
+})
 
+// 规则数据对象
+const rules = {
+  account: [
+    { required: true, message: '用户名不能为空', trigger: 'blur'}
+  ],
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur' },
+    { min: 6, max: 24, message: '密码长度要求6-14个字符', trigger: 'blur' }
+  ],
+  agree: [
+    {
+      validator: (rule, val, callback) => {
+        return val ? callback() : new Error('请先同意协议')
+      }
+    }
+  ]
+}
 </script>
 
 
@@ -24,13 +47,13 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account"/>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password"/>
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox  size="large">

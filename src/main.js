aa978f21598ce7d97369e49520d337ef2b3231a7
yyR <3359754420@qq.com>
2025-03-04 18:16:40 +0800

    详情-全局组件插件化

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import '@/styles/common.scss'
 
 // 引入懒加载指令插件并注册
 import { lazyPlugin } from '@/directives'
+// 引入全局组件插件
+import { componentPlugin } from '@/components'
 
 const app = createApp(App)
 
@@ -17,3 +19,4 @@ app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
 app.mount('#app')
+app.use(componentPlugin)

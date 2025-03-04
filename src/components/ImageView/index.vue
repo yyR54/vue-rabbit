f3d0630cb679aafc5a1bd7f597aa819de7ec115a
yyR <3359754420@qq.com>
2025-03-04 17:54:55 +0800

    大图显示

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -77,7 +77,7 @@ watch([elementX, elementY, isOutside], () => {
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
+        backgroundImage: `url(${imageList[activeIndex]})`,
         backgroundPositionX: `${positionX}px`,
         backgroundPositionY: `${positionY}px`,
       },

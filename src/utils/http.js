006f7eec3654d247bc1de61e1cd66886070b8ebc
yyR <3359754420@qq.com>
2025-03-04 21:03:42 +0800

    登录-token 401拦截器处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import {useUserStore} from '@/stores/user'
+import router from '@/router'
 // 创建axios实例
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -22,11 +23,19 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
   // 统一错误提示
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+  // 401 token失效
+  // 1.清楚本地用户数据
+  // 2.跳转到登录页
+  if(e.response.status === 401) {
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 

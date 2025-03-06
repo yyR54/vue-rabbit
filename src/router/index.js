c3fba07c84777ed2a66cb94577280055fc57fea2
yyR <3359754420@qq.com>
2025-03-06 12:22:47 +0800

    购物车-列表购物车基础渲染

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
-
+import CartList from '@/views/CartList/index.vue'
 
 
 const router = createRouter({
@@ -36,6 +36,10 @@ const router = createRouter({
         {
           path: 'detail/:id',
           component: Detail
+        },
+        {
+          path: 'cartlist',
+          component: CartList
         }
       ]
     },

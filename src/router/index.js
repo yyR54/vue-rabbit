5610c8f6f0c2bfc2b8617e322c5167ffb757d17f
yyR <3359754420@qq.com>
2025-03-07 17:07:53 +0800

    会员中心-功能理解以及路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
-
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 
 const router = createRouter({
@@ -57,6 +59,20 @@ const router = createRouter({
           path: 'paycallback', // 注意路径，必须是paycallback
           component: PayBack
         },
+        {
+          path: '/member',
+          component: Member,
+          children: [
+            {
+              path: 'user',
+              component: UserInfo
+            },
+            {
+              path: 'order',
+              component: UserOrder
+            }
+          ]
+        }
       ]
     },
     {

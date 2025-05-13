ee71226acb8555921c0eb4c95bc998ba50a1dfad
yyR54 <3359754420@qq.com>
2025-05-13 11:36:36 +0800

    添加github部署配置

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,8 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 基础路径，这里使用仓库名作为基础路径
+  base: '/vue-rabbit/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -39,6 +41,30 @@ export default defineConfig({
           @use "@/styles/element/index.scss" as *;
           @use "@/styles/var.scss" as *;
         `,
+      }
+    }
+  },
+  // 生产环境配置
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    // 小于此阈值的导入或引用资源将内联为 base64 编码
+    assetsInlineLimit: 4096,
+    // 启用/禁用 CSS 代码拆分
+    cssCodeSplit: true,
+    // 构建后是否生成 source map 文件
+    sourcemap: false,
+    // 自定义底层的 Rollup 打包配置
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: '[ext]/[name]-[hash].[ext]',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
       }
     }
   }
